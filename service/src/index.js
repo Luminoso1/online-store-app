cf9b41d7e289677daab1b0f7288f1b098341b2e8
Luminoso1 <novoaavila16gmail.com>
2026-03-06 10:15:03 -0500

    cors config allowed origins

--- service/src/index.js
+++ service/src/index.js
@@ -1,19 +1,31 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import router from './routes/index.js'
-import { PORT } from './config.js'
-const app = express()
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import router from "./routes/index.js";
+import { PORT } from "./config.js";
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
+
 app.use(
   cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-  })
-)
-app.use(cookieParser())
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
 
-app.use('/api/v1', router)
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("No permitido por CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
 
-app.listen(PORT, () => console.log('app listen on port 3000'))
+app.use("/api/v1", router);
+
+app.listen(PORT, () => console.log("app listen on port 3000"));
