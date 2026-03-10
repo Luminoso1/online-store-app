0896e704ef5925521081158e4c7b3d3d058dbf9e
Luminoso1 <novoaavila16gmail.com>
2026-03-09 22:13:35 -0500

    suspense & products fallback at home

--- client/src/loaders/products.jsx
+++ client/src/loaders/products.jsx
@@ -1,48 +1,47 @@
-import { getAll, getByCategoryAndName } from '../api/products'
+import { getAll, getByCategoryAndName } from "../api/products";
 
 export const filteredProductsLoader = async ({ params }) => {
   try {
-    const category = params.productsCategory ?? ''
+    const category = params.productsCategory ?? "";
 
-    const { products } = await getAll()
-
-    const filtered =
-      category === 'all' || category === ''
+    const productsPromise = getAll().then(({ products }) => {
+      return category === "all" || category === ""
         ? products
-        : products.filter((p) => p.category === category.toUpperCase())
+        : products.filter((p) => p.category === category.toUpperCase());
+    });
 
-    return { products: filtered }
+    return { products: productsPromise };
   } catch (error) {
-    console.error('Error fetching products. ', error.message)
-    console.log('ERROR HERE')
-    throw new Error(error.message)
+    console.error("Error fetching products. ", error.message);
+    console.log("ERROR HERE");
+    throw new Error(error.message);
   }
-}
+};
 
 export const productsLoader = async ({ request }) => {
-  const url = new URL(request.url)
-  const page = Number(url.searchParams.get('page')) || 1
-  const size = Number(url.searchParams.get('size')) || 5
+  const url = new URL(request.url);
+  const page = Number(url.searchParams.get("page")) || 1;
+  const size = Number(url.searchParams.get("size")) || 5;
   try {
-    const { products, pagination } = await getAll(page, size)
+    const { products, pagination } = await getAll(page, size);
 
-    return { products, pagination }
+    return { products, pagination };
   } catch (error) {
-    console.error('Error fetching products. ', error.message)
-    throw new Error(error.message)
+    console.error("Error fetching products. ", error.message);
+    throw new Error(error.message);
   }
-}
+};
 
 export const productLoader = async ({ params }) => {
   try {
-    const category = params.productsCategory
-    const name = params.productName
+    const category = params.productsCategory;
+    const name = params.productName;
 
-    const { product } = await getByCategoryAndName(category, name)
+    const { product } = await getByCategoryAndName(category, name);
 
-    return { product: product }
+    return { product: product };
   } catch (error) {
-    console.error('Error fetching product. ', error.message)
-    throw new Error(error.message)
+    console.error("Error fetching product. ", error.message);
+    throw new Error(error.message);
   }
-}
+};
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,9 +1,19 @@
-import { Categories } from '../components/categories'
-import { ProductGrid } from '../components/product-grid'
-import { useLoaderData } from 'react-router-dom'
+import { Suspense } from "react";
+import { Categories } from "../components/categories";
+import { ProductGrid } from "../components/product-grid";
+import { useLoaderData, Await } from "react-router-dom";
+import {
+  Grid,
+  Card,
+  CardHeader,
+  CardBody,
+  Stack,
+  Box,
+  Skeleton,
+} from "@chakra-ui/react";
 
 export default function Home() {
-  const { products } = useLoaderData()
+  const data = useLoaderData();
 
   return (
     <>
@@ -13,7 +23,39 @@ export default function Home() {
 
       {/* products */}
 
-      <ProductGrid products={products} />
+      <Suspense fallback={<ProductsFallback />}>
+        <Await resolve={data.products}>
+          {(products) => <ProductGrid products={products} />}
+        </Await>
+      </Suspense>
     </>
-  )
+  );
 }
+
+const ProductsFallback = () => {
+  return (
+    <Grid
+      templateColumns="repeat(auto-fit,minmax(240px, 1fr))"
+      rowGap="10"
+      columnGap="6"
+      justifyContent="space-between"
+      mt="20"
+    >
+      {Array.from({ length: 8 }).map((index) => (
+        <Card key={index} w="full" pb="4">
+          <CardHeader>
+            <Box height="44" overflow="hidden">
+              <Skeleton height="100%" width="100%" />
+            </Box>
+          </CardHeader>
+
+          <CardBody>
+            <Stack spacing="3">
+              <Skeleton height="24px" width="50%" />
+            </Stack>
+          </CardBody>
+        </Card>
+      ))}
+    </Grid>
+  );
+};
