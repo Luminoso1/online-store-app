bec621991e055260ab428122acab6e00894f5bf6
Luminoso1 <novoaavila16gmail.com>
2026-03-12 11:55:32 -0500

    feat: add Dockerfile & use PORT 8080

--- service/src/config.js
+++ service/src/config.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const PORT = process.env.PORT || 3000
+export const PORT = process.env.PORT || 8080
 
 export const SECRET_TOKEN_KEY = process.env.SECRET_TOKEN_KEY
 
